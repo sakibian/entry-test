6963fab9d3131414b3123e17f81e429fe63bc873
Andres Cajamarca <afcajamarcar@unal.edu.co>
2025-05-16 15:54:39 -0400

    add cypress

---
+++ cypress/support/e2e.js
@@ -0,0 +1,9 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Import testing-library commands
+import '@testing-library/cypress/add-commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+// require('@testing-library/cypress/add-commands') 
