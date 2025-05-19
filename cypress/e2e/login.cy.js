1b0d9c71bd409573edb609d2c2a8292ada3db598
sakibian <sakib.works@gmail.com>
2025-05-19 16:55:37 +0600

    Update tests and login form

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,31 @@
 describe('Login Component', () => {
-}) 
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should display the login form initially', () => {
+    cy.get('.login-form').should('be.visible');
+    cy.get('input[name="name"]').should('be.visible');
+    cy.get('input[name="password"]').should('be.visible');
+    cy.get('button[type="submit"]').contains('Login').should('be.visible');
+  });
+
+  it('should allow user to log in and display welcome message', () => {
+    cy.get('input[name="name"]').type('John Doe');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.welcome-container').should('be.visible');
+    cy.get('.welcome-card h1').should('contain', 'Welcome, John Doe!');
+    cy.get('.logout-button').should('be.visible');
+  });
+
+  it('should allow user to log out and return to login form', () => {
+    cy.get('input[name="name"]').type('John Doe');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.logout-button').click();
+    cy.get('.login-form').should('be.visible');
+    cy.get('input[name="name"]').should('have.value', '');
+    cy.get('input[name="password"]').should('have.value', '');
+  });
+});
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,9 +15,14 @@ function LoginForm({ onLogin }) {
     }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(formData);
+  };
+
   return (
     <div className="login-form-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
@@ -49,4 +54,4 @@ function LoginForm({ onLogin }) {
   );
 }
 
-export default LoginForm; 
+export default LoginForm;
